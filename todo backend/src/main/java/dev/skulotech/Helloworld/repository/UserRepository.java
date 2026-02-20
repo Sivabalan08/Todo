6f55bf015920e3ff6adf693e4aa7222eb1994c69
Sivabalan08 <142318540+Sivabalan08@users.noreply.github.com>
2026-02-20 12:38:05 +0530

    Delete todo backend directory

--- todo backend/src/main/java/dev/skulotech/Helloworld/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.skulotech.Helloworld.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import dev.skulotech.Helloworld.models.User;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByEmail(String email);
-}
